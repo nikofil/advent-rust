8ae176b4828e830bfc62de3c442e775c458b54a9
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2018-12-20 13:29:03 +0100

    Day 13 - 1
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,14 @@ pub fn read_line() -> Option<String> {
         None
     }
 }
+
+pub fn read_no_trim() -> Option<String> {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).ok()?;
+    buf = buf.to_string();
+    if buf.len() > 0 {
+        Some(buf)
+    } else {
+        None
+    }
+}
