927c1267272cf1716a22ac5a2bd091504c9e3fc7
Nikos Filippakis <nikolaos.filippakis@cern.ch>
2018-12-21 13:36:18 +0100

    Day 15 - 1
    Signed-off-by: Nikos Filippakis <nikolaos.filippakis@cern.ch>

--- src/bin/day14_1.rs
+++ src/bin/day14_1.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let r: usize = 084601;
+    let r = [0usize, 8, 4, 6, 0, 1];
     let mut v = vec![3usize, 7];
     let mut x = 0usize;
     let mut y = 1usize;
-    while v.len() < r+10 {
+    let mut i = 0;
+    loop {
+        i += 1;
         let nxt = v[x] + v[y];
         if nxt >= 10 {
             v.push(nxt / 10);
@@ -11,6 +13,10 @@ fn main() {
         v.push(nxt % 10);
         x = (x + 1 + v[x]) % v.len();
         y = (y + 1 + v[y]) % v.len();
+        let l = v.len();
+        if l > 10 && (v[l-10..l] == r) {
+
+        }
     }
-    println!("{:?}", &v[r..r+10]);
+    println!("{}", i);
 }
